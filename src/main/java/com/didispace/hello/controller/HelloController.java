fecdaa02e251e84732ac1060c955ca6b38c12b16
xjx <xjx@qq.com>
2018-11-14 17:56:02 +0800

    xjx

--- src/main/java/com/didispace/hello/controller/HelloController.java
+++ src/main/java/com/didispace/hello/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.didispace.hello.controller;
 
 import com.didispace.hello.properties.BookProperties;
+import com.netflix.discovery.converters.Auto;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.metrics.CounterService;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 /**
  * Created by xjx on 2018/11/12.
@@ -14,19 +19,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private final Logger LOG = Logger.getLogger(HelloController.class);
     @Autowired
     private BookProperties book;
+//    @Autowired
+//    private CounterService counterService;
     @Autowired
-    private CounterService counterService;
+    private DiscoveryClient client;
     @Autowired
-    private DiscoveryClient discoveryclient;
+    private RestTemplate restTemplate;
 
-    @RequestMapping("/hello/{name}")
-    public String index(@PathVariable String name) {
-        counterService.increment("name " + name);
-//        System.out.println(name);
-//        System.out.println(book.getAuthor()+" "+book.getName()+" "+book.getDesc());
-//        System.out.println(book.getA()+" "+book.getB()+" "+book.getC());
-        return "你好 "+name;
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String index() throws Exception{
+        ServiceInstance instance = client.getLocalServiceInstance();
+        //让处理线程等上几秒钟
+        int sleepTime = new Random().nextInt(3000);
+        LOG.info("sleepTime: "+sleepTime);
+        Thread.sleep(sleepTime);
+
+        LOG.info("/hello, host:"+instance.getHost()+", service_id:"+instance.getServiceId());
+        return "hello springcloud";
     }
+
+    @RequestMapping("/index2")
+    public String index2(){
+        return restTemplate.getForEntity("http://RIBBON-CONSUMER/helloConsumer2",String.class).getBody();
+    }
+
 }
