e1d55c8e1319e71aa070d64335aeadc1d284cc92
xjx <xjx@qq.com>
2018-11-15 18:08:41 +0800

    xjx

--- src/main/java/com/didispace/hello/controller/HelloController.java
+++ src/main/java/com/didispace/hello/controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.didispace.hello.controller;
 
+import com.didispace.hello.pojo.User;
 import com.didispace.hello.properties.BookProperties;
 import com.netflix.discovery.converters.Auto;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
@@ -22,28 +21,43 @@ public class HelloController {
     private final Logger LOG = Logger.getLogger(HelloController.class);
     @Autowired
     private BookProperties book;
-//    @Autowired
+    //    @Autowired
 //    private CounterService counterService;
     @Autowired
     private DiscoveryClient client;
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String index() throws Exception{
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() throws Exception {
         ServiceInstance instance = client.getLocalServiceInstance();
         //让处理线程等上几秒钟
-        int sleepTime = new Random().nextInt(3000);
-        LOG.info("sleepTime: "+sleepTime);
-        Thread.sleep(sleepTime);
+//        int sleepTime = new Random().nextInt(3000);
+//        LOG.info("sleepTime: "+sleepTime);
+//        Thread.sleep(sleepTime);
 
-        LOG.info("/hello, host:"+instance.getHost()+", service_id:"+instance.getServiceId());
+        LOG.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
         return "hello springcloud";
     }
 
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "hello " + name;
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
+        return new User(name, age);
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String hello(@RequestBody User user) {
+        return "hello " + user.getName() + ", " + user.getAge();
+    }
+
     @RequestMapping("/index2")
-    public String index2(){
-        return restTemplate.getForEntity("http://RIBBON-CONSUMER/helloConsumer2",String.class).getBody();
+    public String index2() {
+        return restTemplate.getForEntity("http://RIBBON-CONSUMER/helloConsumer2", String.class).getBody();
     }
 
 }
---
+++ src/main/java/com/didispace/hello/pojo/User.java
@@ -0,0 +1,44 @@
+package com.didispace.hello.pojo;
+
+import java.io.Serializable;
+
+/**
+ * Created by xjx on 2018/11/15.
+ */
+public class User implements Serializable {
+
+    private String name;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
